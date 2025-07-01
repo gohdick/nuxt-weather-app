d8ee3e892e403aa8f1c5bcc9fc2920ffd5cc2fdb
Teerasak Simtong <thirasaksimtong@gmail.com>
2025-07-01 17:49:09 +0700

    insert all file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,19 @@ export default defineNuxtConfig({
   modules: ['nuxt-toast', '@nuxtjs/tailwindcss'],
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
+  app: {
+    head: {
+      title: 'WeatherCast',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: '' },
+        { name: 'format-detection', content: 'telephone=no' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/weather-icon.svg' },
+      ],
+    },
+  },
 
 })
