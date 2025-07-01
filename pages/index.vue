03249bfc7ecdc0aa6ce4752c6053e3de017d0423
Teerasak Simtong <thirasaksimtong@gmail.com>
2025-07-01 17:52:03 +0700

    insert all file

--- pages/index.vue
+++ pages/index.vue
@@ -237,7 +237,7 @@
       
       <!-- Footer -->
       <div class="mt-8 text-center text-white/70 dark:text-white/50 text-sm">
-        <p>© {{ new Date().getFullYear() }} WeatherCast | บริการข้อมูลสภาพอากาศคุณภาพสูง พัฒนาโดย Mr.Thirasak Simtong</p>
+        <p>© {{ new Date().getFullYear() }} WeatherCast | <a href="https://openweathermap.org" target="_blank" rel="noopener noreferrer">https://openweathermap.org</a>  <br>บริการข้อมูลสภาพอากาศคุณภาพสูง | พัฒนาโดย Mr.Thirasak Simtong</p>
       </div>
     </div>
   </div>
