294046bd68a7a1a67e16a7a286649d2d859765ba
gohdick <thirasaksimtong@gmail.com>
2025-07-01 20:35:50 +0700

    Update project all file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,8 @@ export default defineNuxtConfig({
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'description', content: '' },
+        { name: 'description', content: 'WeatherCast is a modern weather forecasting application built with Nuxt.js. It provides users with real-time weather information, forecasts, and weather-related data in a clean, intuitive interface.' },
+        { property: 'og:description', content: 'WeatherCast เป็นแอปพลิเคชันพยากรณ์อากาศที่ทันสมัยสร้างด้วย Nuxt.js ให้ข้อมูลสภาพอากาศแบบเรียลไทม์ การพยากรณ์ และข้อมูลที่เกี่ยวข้องกับสภาพอากาศในอินเทอร์เฟซที่สะอาดและใช้งานง่าย' },
         { name: 'format-detection', content: 'telephone=no' },
       ],
       link: [
